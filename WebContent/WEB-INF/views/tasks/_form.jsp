6178fef37eb65fda856adf5e959c89c8d08aa656
hikaru.ishizawa <iszwhkr89.it@gmail.com>
2022-11-09 00:28:34 +0900

    CreateServlet.javaを作成

---
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,10 @@
+<!-- new と edit　で同じフォームを利用したいため共通ファイル化 -->
+
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+<label for="content_tsk">タスク</label><br />
+<input type="text" name="content" id="content_tsk" value="${task.content}" />
+<br /><br />
+
+<input type="hidden" name="_token" value="${_token}" />
+<button type="submit">投稿</button>
--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -33,15 +34,16 @@ public class IndexServlet extends HttpServlet {
          */
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        //EntityManagerのオブジェクトを生成
         EntityManager em = DBUtil.createEntityManager();
 
-        //Task.javaでJPQLにつけた名前「getAllTasks」をcreateNamedQueryメソッドの引数に指定することでDBへの問い合わせを実行
-        //上記の問い合わせ結果をgetResultList()メソッドでリスト形式で取得・・・DBに保存されたデータはHibernateによって自動でMessageクラスのオブジェクトになってこのリストの中に格納される
         List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
-        response.getWriter().append(Integer.valueOf(tasks.size()).toString());
 
         em.close();
+
+        request.setAttribute("tasks", tasks);
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
+        rd.forward(request, response);
     }
 
 }
